bd6de851eea4ffe9afe9b0163b614242b40d97f3
Andre6925 <agoiquatrin@gmail.com>
2026-08-05 22:40:07 +0000

    multiplicação

--- Ex1.java
+++ Ex1.java
@@ -3,4 +3,12 @@ public Ex1{
     public void main(String []args){
         
     }
+
+    public int multiDois(int n1, int n2){
+        if(n1 != 0){
+            return n2 += n2; 
+        }
+        return n2; 
+
+    }
 }
