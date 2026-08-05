6473c5d54673c4c733206c7785dbad33ec79d283
Andre6925 <agoiquatrin@gmail.com>
2026-08-05 23:16:54 +0000

    ad

--- Ex1.java
+++ Ex1.java
@@ -2,10 +2,7 @@
 public class Ex1{
 
     public static void main(String []args){
-
-        System.out.println(multiDois(6, 1));
-       // System.out.println(Func(10));
-   
+        System.out.println(multiDois(6, 1));   
     }
 
     public static int multiDois(int n1, int n2){
@@ -17,11 +14,5 @@ public class Ex1{
         return n2; 
     }
 
-    public static int Func(int n){
-        if(n != 0){
-         return n = 2 * Func(n - 1) + 3 * Func(n - 2);
-        }
-        return n; 
-    }
-
+   
 }
